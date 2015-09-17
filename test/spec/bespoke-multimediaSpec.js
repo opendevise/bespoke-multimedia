984f1cb4f15a778cb80ab49f1477fc6d185e91f5
Dan Allen <dan.j.allen@gmail.com>
2015-09-17 02:04:36 -0600

    fix test descriptions

--- test/spec/bespoke-multimediaSpec.js
+++ test/spec/bespoke-multimediaSpec.js
@@ -3,7 +3,7 @@ Function.prototype.bind = Function.prototype.bind || require('function-bind');
 var bespoke = require('bespoke'),
   multimedia = require('../../lib/bespoke-multimedia.js');
 
-describe("bespoke-multimedia", function() {
+describe('bespoke-multimedia', function() {
 
   var deck,
     parent,
@@ -52,25 +52,22 @@ describe("bespoke-multimedia", function() {
   beforeEach(createDeck);
   afterEach(destroyDeck);
 
-  describe("deck.slide", function() {
+  describe('video playback', function() {
+    beforeEach(function() { deck.slide(0); });
 
-    beforeEach(function() {
-      deck.slide(0);
-    });
-
-    it("should play video on first slide", function() {
+    it('should play video on first slide', function() {
       var video = deck.parent.querySelectorAll('video')[0];
       expect(video.paused).toBe(false);
     });
 
-    it("should play video when slide is activated", function() {
+    it('should play video when slide is activated', function() {
       var video = deck.parent.querySelectorAll('video')[1];
       expect(video.paused).toBe(true);
       deck.next();
       expect(video.paused).toBe(false);
     });
 
-    it("should pause video when slide is deactivated", function() {
+    it('should pause video when slide is deactivated', function() {
       deck.next();
       deck.next();
       var video0 = deck.parent.querySelectorAll('video')[0];
@@ -78,9 +75,13 @@ describe("bespoke-multimedia", function() {
       var video1 = deck.parent.querySelectorAll('video')[1];
       expect(video1.paused).toBe(true);
     });
+  });
+
+  describe('SVG animation', function() {
+    beforeEach(function() { deck.slide(0); });
 
     // NOTE this doesn't really test anything, other than looking for the SVG doesn't break the plugin
-    it("should animate SVG when slide is activated", function() {
+    it('should animate SVG when slide is activated', function() {
       deck.next();
       deck.next();
       deck.next();
