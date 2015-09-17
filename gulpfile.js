ab374be1a07686c821ad52a0cec4f640b3949528
Dan Allen <dan.j.allen@gmail.com>
2015-09-17 02:11:29 -0600

    add tasks to publish demo

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp'),
   pkg = require('./package.json'),
   browserify = require('browserify'),
   buffer = require('vinyl-buffer'),
+  concat = require('gulp-concat'),
   del = require('del'),
   header = require('gulp-header'),
   jshint = require('gulp-jshint'),
   karma = require('karma'),
+  pages = require('gh-pages'),
   rename = require('gulp-rename'),
+  replace = require('gulp-replace'),
   source = require('vinyl-source-stream'),
   uglify = require('gulp-uglify');
 
@@ -19,7 +22,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('clean', function(done) {
-  del(['dist', 'test/coverage'], done);
+  del(['dist', 'demo/dist', 'test/coverage'], done);
 });
 
 gulp.task('lint', function() {
@@ -58,4 +61,40 @@ gulp.task('compile', ['clean'], function() {
       '<%= license %> License */\n'
     ].join(''), pkg))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('compile:demo', ['compile:demo:css', 'compile:demo:assets', 'compile:demo:html', 'compile:demo:js']);
+
+gulp.task('compile:demo:css', ['clean'], function() {
+  return gulp.src('demo/style.css')
+    .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('compile:demo:assets', ['clean'], function() {
+  return gulp.src('demo/*.{svg,mp4}')
+    .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('compile:demo:html', ['clean'], function() {
+  return gulp.src('demo/index.html')
+    .pipe(replace(/<script [\s\S]*<\/script>/, '<script src="build.js"></script>'))
+    .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('compile:demo:js', ['compile'], function() {
+  return gulp.src([
+      'node_modules/bespoke/dist/bespoke.js',
+      'node_modules/bespoke-classes/dist/bespoke-classes.js',
+      'node_modules/bespoke-keys/dist/bespoke-keys.js',
+      'node_modules/bespoke-touch/dist/bespoke-touch.js',
+      'dist/bespoke-multimedia.js',
+      'demo/demo.js'
+  ])
+  .pipe(concat('build.js'))
+  //.pipe(uglify())
+  .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('deploy', ['compile:demo'], function(done) {
+  pages.publish('demo/dist', {}, done);
 });
