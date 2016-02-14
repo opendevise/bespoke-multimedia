56cd2c9371542c2dfb3257c6cc2c0461e70e5867
Dan Allen <dan.j.allen@gmail.com>
2016-02-14 03:51:37 -0700

    toggle active class on SVG by default; reload if data-reload attribute is set

--- dist/bespoke-multimedia.js
+++ dist/bespoke-multimedia.js
@@ -15,15 +15,15 @@ module.exports = function() {
       forEach = function(c, fn) {
         Array.prototype.forEach.call(c, fn);
       },
-      findAnimatedSvgs = function(scope) {
-        // QUESTION should we require an "animated" class on the object element?
-        return scope.querySelectorAll('object[type="image/svg+xml"]');
-      },
       findGifs = function(scope) {
         return scope.querySelectorAll('img[src*=".gif"]');
       },
       findMedia = function(scope) {
         return scope.querySelectorAll('audio,video,iframe[src*="//www.youtube.com/embed/"],iframe[src*="//player.vimeo.com/"]');
+      },
+      findSvgs = function(scope, specifier) {
+        // QUESTION should we require an "animated" class on the object element?
+        return scope.querySelectorAll('object[type="image/svg+xml"]' + (specifier === undefined ? '' : specifier));
       },
       playMedia = function(obj) {
         var volume = obj.hasAttribute('data-volume') ? parseInt(obj.getAttribute('data-volume'), 10) : null;
@@ -68,24 +68,50 @@ module.exports = function() {
       pauseAllMedia = function(slide) {
         forEach(findMedia(slide), pauseMedia);
       },
-      // NOTE doesn't work reliably in Firefox
-      restartSvgAnimation = function(obj) {
-        // NOTE obj.data resolves to the qualified URL; getAttribute('data') returns original value
-        obj.data = obj.getAttribute('data'); // setting data forces element to reload, restarting SVG animation
+      activateSvg = function(obj) {
+        if (obj.getAttribute('data-reload') === 'true') {
+          reloadSvg(obj);
+        }
+        else {
+          var svg = obj.contentDocument.documentElement;
+          if (svg.tagName === 'svg') {
+            svg.classList.add('active');
+          }
+          else {
+            obj.onload = function() { // wait for the SVG to load into the object container
+              this.onload = null;
+              if (deck.slides[deck.slide()].contains(this)) {
+                this.contentDocument.documentElement.classList.add('active');
+              }
+            };
+          }
+        }
       },
-      restartSvgAnimations = function(slide) {
-        forEach(findAnimatedSvgs(slide), restartSvgAnimation);
+      reloadSvg = function(obj) {
+        // NOTE obj.data resolves to the qualified URL while getAttribute('data') returns original value
+        obj.data = obj.getAttribute('data'); // setting data forces element to reload, replaying autostart animations
       },
-      restartGifAnimation = function(img) {
-        // NOTE obj.src resolves to the qualified URL; getAttribute('src') returns original value
+      activateSvgs = function(slide) {
+        forEach(findSvgs(slide), activateSvg);
+      },
+      reloadGif = function(img) {
+        // NOTE obj.src resolves to the qualified URL while getAttribute('src') returns original value
         img.src = img.getAttribute('src'); // setting src forces element to reload, restarting GIF animation
       },
-      restartGifAnimations = function(slide) {
-        forEach(findGifs(slide), restartGifAnimation);
+      reloadGifs = function(slide) {
+        forEach(findGifs(slide), reloadGif);
       },
       restartAnimations = function(slide) {
-        restartSvgAnimations(slide);
-        restartGifAnimations(slide);
+        activateSvgs(slide);
+        reloadGifs(slide);
+      },
+      deactivateSvg = function(obj) {
+        obj.onload = null;
+        var svg = obj.contentDocument.documentElement;
+        if (svg.tagName === 'svg') svg.classList.remove('active');
+      },
+      deactivateSvgs = function(slide) {
+        forEach(findSvgs(slide, ':not([data-reload="true"])'), deactivateSvg);
       },
       activate = function(e) {
         if (e.preview) {
@@ -98,6 +124,7 @@ module.exports = function() {
       },
       deactivate = function(e) {
         pauseAllMedia(e.slide);
+        deactivateSvgs(e.slide);
       };
     deck.on('activate', activate);
     deck.on('deactivate', deactivate);
