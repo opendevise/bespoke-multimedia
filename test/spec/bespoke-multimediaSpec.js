0772db17e73231cb9282e89c77d35205e8bfeb35
Dan Allen <dan.j.allen@gmail.com>
2016-02-14 00:58:13 -0700

    add tests for audio

--- test/spec/bespoke-multimediaSpec.js
+++ test/spec/bespoke-multimediaSpec.js
@@ -9,26 +9,35 @@ describe('bespoke-multimedia', function() {
     parent,
     createDeck = function() {
       parent = document.createElement('article');
-      for (var i = 0; i < 4; i++) {
+      for (var i = 1; i <= 5; i++) {
         var section = document.createElement('section');
-        section.id = 'slide-' + (i + 1)
-        if (i < 2) {
+        section.id = 'slide-' + i
+        if (i < 3) {
           var video = document.createElement('video');
           video.setAttribute('src', 'asset/sample.webm');
           video.setAttribute('loop', 'true');
           video.setAttribute('preload', 'auto');
           if (typeof video.play !== 'function') {
             video.paused = true;
-            video.play = function() {
-              this.paused = false;
-            };
-            video.pause = function() {
-              this.paused = true;
-            };
+            video.play = function() { this.paused = false; };
+            video.pause = function() { this.paused = true; };
           }
           section.appendChild(video); 
         }
-        if (i == 2) {
+        else if (i == 3) {
+          var audio = document.createElement('audio');
+          audio.setAttribute('src', 'asset/sample.ogg');
+          audio.setAttribute('loop', 'true');
+          audio.setAttribute('preload', 'auto');
+          audio.setAttribute('data-volume', '0');
+          if (typeof audio.play !== 'function') {
+            audio.paused = true;
+            audio.play = function() { this.paused = false; };
+            audio.pause = function() { this.paused = true; };
+          }
+          section.appendChild(audio); 
+        }
+        else if (i == 4) {
           var object = document.createElement('object');
           object.setAttribute('type', 'image/svg+xml');
           object.setAttribute('data', 'asset/sample.svg');
@@ -44,9 +53,7 @@ describe('bespoke-multimedia', function() {
       ]);
     },
     destroyDeck = function() {
-      if (parent != null) {
-        parent.parentNode.removeChild(parent);
-      }
+      if (parent != null) parent.parentNode.removeChild(parent);
     };
 
   beforeEach(createDeck);
@@ -77,13 +84,32 @@ describe('bespoke-multimedia', function() {
     });
   });
 
+  describe('audio playback', function() {
+    beforeEach(function() { deck.slide(0); });
+
+    it('should play audio when slide is activated', function() {
+      var audio = deck.parent.querySelectorAll('audio')[0];
+      deck.slide(1);
+      expect(audio.paused).toBe(true);
+      deck.next();
+      expect(audio.paused).toBe(false);
+    });
+
+    it('should pause audio when slide is deactivated', function() {
+      var audio = deck.parent.querySelectorAll('audio')[0];
+      deck.slide(2);
+      expect(audio.paused).toBe(false);
+      deck.next();
+      expect(audio.paused).toBe(true);
+    });
+  });
+
   describe('SVG animation', function() {
     beforeEach(function() { deck.slide(0); });
 
     // NOTE this doesn't really test anything, other than looking for the SVG doesn't break the plugin
     it('should animate SVG when slide is activated', function() {
-      deck.next();
-      deck.next();
+      deck.slide(2);
       deck.next();
       expect(deck.parent.querySelector('object').getAttribute('data')).toBe('asset/sample.svg');
     });
