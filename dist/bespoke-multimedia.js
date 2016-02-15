174945f6dead2fbc7f8901d5a3aebad9564b9972
Dan Allen <dan.j.allen@gmail.com>
2016-02-14 19:39:14 -0700

    consolidate code

--- dist/bespoke-multimedia.js
+++ dist/bespoke-multimedia.js
@@ -8,12 +8,14 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.multimedia = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = function() {
   return function(deck) {
-    var VIMEO_URI_RE = /\/\/player\.vimeo\.com\//,
-      YOUTUBE_URI_RE = /\/\/www\.youtube\.com\/embed\//,
+    var VIMEO_URI_RE = /\/\/player\.vimeo\.com\//, YOUTUBE_URI_RE = /\/\/www\.youtube\.com\/embed\//,
       PLAYBACK_DENIED_MSG = 'Access denied: Cannot automatically play %s video since deck is loaded from a file:// URI.',
-      isLocal = window.location.protocol === 'file:',
+      local = window.location.protocol === 'file:',
       forEach = function(c, fn) {
         Array.prototype.forEach.call(c, fn);
+      },
+      postMessage = function(obj, msg) {
+        obj.contentWindow.postMessage(JSON.stringify(msg), '*');
       },
       findGifs = function(scope) {
         return scope.querySelectorAll('img[src*=".gif"]');
@@ -22,31 +24,26 @@ module.exports = function() {
         return scope.querySelectorAll('audio,video,iframe[src*="//www.youtube.com/embed/"],iframe[src*="//player.vimeo.com/"]');
       },
       findSvgs = function(scope, specifier) {
-        // QUESTION should we require an "animated" class on the object element?
-        return scope.querySelectorAll('object[type="image/svg+xml"]' + (specifier === undefined ? '' : specifier));
+        return scope.querySelectorAll('object[type="image/svg+xml"]' + (specifier || ''));
       },
       playMedia = function(obj) {
-        var volume = obj.hasAttribute('data-volume') ? parseInt(obj.getAttribute('data-volume'), 10) : null;
-        if (volume !== null) {
-          if (volume < 0) volume = 0;
-          if (volume > 100) volume = 100;
-        }
+        var vol = obj.hasAttribute('data-volume') ? Math.max(Math.min(parseFloat(obj.getAttribute('data-volume')), 100), 0) : null;
         if (typeof obj.play === 'function') {
           if (obj.paused === true) {
-            if (volume !== null) obj.volume = (volume / 100);
+            if (vol !== null) obj.volume = vol/100;
             obj.play();
           }
         }
-        else if (typeof obj.src === 'string') {
+        else if (obj.hasAttribute('src')) {
           if (VIMEO_URI_RE.test(obj.src)) {
-            if (isLocal) return console.warn(PLAYBACK_DENIED_MSG.replace(/%s/, 'Vimeo'));
-            if (volume !== null) obj.contentWindow.postMessage('{ "method": "setVolume", "value": ' + (volume / 100) + ' }', '*');
-            obj.contentWindow.postMessage('{ "method": "play" }', '*');
+            if (local) return console.warn(PLAYBACK_DENIED_MSG.replace(/%s/, 'Vimeo'));
+            if (vol !== null) postMessage(obj, {'method': 'setVolume', 'value': vol/100});
+            postMessage(obj, {'method': 'play'});
           }
           else if (YOUTUBE_URI_RE.test(obj.src)) {
-            if (isLocal) return console.warn(PLAYBACK_DENIED_MSG.replace(/%s/, 'YouTube'));
-            if (volume !== null) obj.contentWindow.postMessage('{ "event": "command", "func": "setVolume", "args": [' + volume + '] }', '*');
-            obj.contentWindow.postMessage('{ "event": "command", "func": "playVideo" }', '*');
+            if (local) return console.warn(PLAYBACK_DENIED_MSG.replace(/%s/, 'YouTube'));
+            if (vol !== null) postMessage(obj, {'event': 'command', 'func': 'setVolume', 'args': [vol]});
+            postMessage(obj, {'event': 'command', 'func': 'playVideo'});
           }
         }
       },
@@ -54,13 +51,9 @@ module.exports = function() {
         if (typeof obj.pause === 'function') {
           if (obj.paused === false) obj.pause();
         }
-        else if (typeof obj.src === 'string' && !isLocal) {
-          if (VIMEO_URI_RE.test(obj.src)) {
-            obj.contentWindow.postMessage('{ "method": "pause" }', '*');
-          }
-          else if (YOUTUBE_URI_RE.test(obj.src)) {
-            obj.contentWindow.postMessage('{ "event": "command", "func": "pauseVideo" }', '*');
-          }
+        else if (obj.hasAttribute('src') && !local) {
+          if (VIMEO_URI_RE.test(obj.src)) postMessage(obj, {'method': 'pause'});
+          else if (YOUTUBE_URI_RE.test(obj.src)) postMessage(obj, {'event': 'command', 'func': 'pauseVideo'});
         }
       },
       playAllMedia = function(slide) {
@@ -70,33 +63,27 @@ module.exports = function() {
         forEach(findMedia(slide), pauseMedia);
       },
       activateSvg = function(obj) {
-        if (obj.getAttribute('data-reload') === 'true') {
-          reloadSvg(obj);
-        }
+        if (obj.getAttribute('data-reload') === 'true') reloadSvg(obj);
         else {
           var svg = obj.contentDocument.documentElement;
-          if (svg.tagName === 'svg') {
-            svg.classList.add('active');
-          }
+          if (svg.tagName === 'svg') svg.classList.add('active');
           else {
             obj.onload = function() { // wait for the SVG to load into the object container
               this.onload = null;
-              if (deck.slides[deck.slide()].contains(this)) {
-                this.contentDocument.documentElement.classList.add('active');
-              }
+              if (deck.slides[deck.slide()].contains(this)) this.contentDocument.documentElement.classList.add('active');
             };
           }
         }
       },
       reloadSvg = function(obj) {
-        // NOTE obj.data resolves to the qualified URL while getAttribute('data') returns original value
+        // NOTE obj.data returns qualified URL whereas obj.getAttribute('data') returns original value
         obj.data = obj.getAttribute('data'); // setting data forces element to reload, replaying autostart animations
       },
       activateSvgs = function(slide) {
         forEach(findSvgs(slide), activateSvg);
       },
       reloadGif = function(img) {
-        // NOTE obj.src resolves to the qualified URL while getAttribute('src') returns original value
+        // NOTE img.src returns qualified URL whereas img.getAttribute('src') returns original value
         img.src = img.getAttribute('src'); // setting src forces element to reload, restarting GIF animation
       },
       reloadGifs = function(slide) {
@@ -115,9 +102,7 @@ module.exports = function() {
         forEach(findSvgs(slide, ':not([data-reload="true"])'), deactivateSvg);
       },
       activate = function(e) {
-        if (e.preview) {
-          pauseAllMedia(e.slide);
-        }
+        if (e.preview) pauseAllMedia(e.slide);
         else {
           playAllMedia(e.slide);
           restartAnimations(e.slide);
