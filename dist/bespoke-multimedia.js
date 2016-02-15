f98eda61734f402108f378f0b826c34cd6b4e5a3
Dan Allen <dan.j.allen@gmail.com>
2016-02-15 04:39:56 -0700

    add option to restart media on slide entry

--- dist/bespoke-multimedia.js
+++ dist/bespoke-multimedia.js
@@ -8,39 +8,37 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.multimedia = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = function() {
   return function(deck) {
-    var ACTIVE = 'active', VIMEO_RE = /\/\/player\.vimeo\.com\//, YOUTUBE_RE = /\/\/www\.youtube\.com\/embed\//,
-      file = window.location.protocol === 'file:',
+    var ACTIVE = 'active', VIMEO_RE = /\/\/player\.vimeo\.com\//, YOUTUBE_RE = /\/\/www\.youtube\.com\/embed\//, file = window.location.protocol === 'file:',
       post = function(obj, msg) { obj.contentWindow.postMessage(JSON.stringify(msg), '*'); },
       query = function(select, scope, pr) { return Array.prototype.slice.call(scope.querySelectorAll(select + (pr||''))); },
-      findGifs = query.bind(null, 'img[src*=".gif"]'),
+      findGifs = query.bind(null, 'img[src*=".gif"][data-restart=true]'),
       findMedia = query.bind(null, 'audio,video,iframe[src*="//www.youtube.com/embed/"],iframe[src*="//player.vimeo.com/"]'),
       findSvgs = query.bind(null, 'object[type="image/svg+xml"]'),
       reloadGif = function(img) { img.src = img.getAttribute('src'); },
       play = function(obj) {
-        var vol = obj.hasAttribute('data-volume') ? Math.max(Math.min(parseFloat(obj.getAttribute('data-volume')), 10), 0) : NaN;
-        if (typeof obj.play === 'function') {
-          if (obj.paused === true) {
-            if (!isNaN(vol)) obj.volume = vol/10;
-            obj.play();
-          }
+        var vol = obj.hasAttribute('data-volume') ? Math.max(Math.min(parseFloat(obj.getAttribute('data-volume')), 10), 0) : NaN, re = obj.getAttribute('data-restart') === 'true';
+        if (obj.play) {
+          if (re) obj.currentTime = 0;
+          if (!isNaN(vol)) obj.volume = vol/10;
+          if (obj.paused) obj.play();
         }
-        else if (obj.hasAttribute('src')) {
+        else {
           if (YOUTUBE_RE.test(obj.src)) {
+            if (re) post(obj, {event: 'command', func: 'seekTo', args: [0]});
             if (!isNaN(vol)) post(obj, {event: 'command', func: 'setVolume', args: [vol*10]});
             post(obj, {event: 'command', func: 'playVideo'});
           }
           else if (VIMEO_RE.test(obj.src)) {
-            if (file) return console.warn('WARNING: Cannot autoplay Vimeo video since deck is loaded from a file:// URI.');
+            if (file) return console.warn('WARNING: Cannot control Vimeo video since deck is loaded from a file:// URI.');
+            if (re) post(obj, {method: 'seekTo', value: 0});
             if (!isNaN(vol)) post(obj, {method: 'setVolume', value: vol/10});
             post(obj, {method: 'play'});
           }
         }
       },
       pause = function(obj) {
-        if (typeof obj.pause === 'function') {
-          if (obj.paused === false) obj.pause();
-        }
-        else if (obj.hasAttribute('src')) {
+        if (obj.pause) obj.pause();
+        else {
           if (YOUTUBE_RE.test(obj.src)) post(obj, {event: 'command', func: 'pauseVideo'});
           else if (!file && VIMEO_RE.test(obj.src)) post(obj, {method: 'pause'});
         }
@@ -51,8 +49,8 @@ module.exports = function() {
         else {
           var svg = obj.contentDocument.documentElement;
           if (svg.tagName === 'svg') svg.classList.add(ACTIVE);
-          else
-            obj.addEventListener('load', function addActive() { // wait for the SVG to load into object container
+          else // wait for the SVG to load into object container
+            obj.addEventListener('load', function addActive() {
               this.removeEventListener(addActive, false);
               if (deck.slides[deck.slide()].contains(this)) this.contentDocument.documentElement.classList.add(ACTIVE);
             }, false);
